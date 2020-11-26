c063ba6a69a5d662ff2da63c9c61160d9374a101
marangoni <oliveiralcm@gmail.com>
2020-11-26 14:51:01 -0300

    Adaptações da interface

--- balanca_IoT_frontend/js/real-time-database.js
+++ balanca_IoT_frontend/js/real-time-database.js
@@ -1,5 +1,6 @@
 var medsList = document.getElementById('medsList');
 var medsTable= document.getElementById('medsTable');
+var balList = document.getElementById('balList');
 var valorPeso = document.getElementById('valorPeso');
 var medidaEm = document.getElementById('medidaEm');
 var balancaId = document.getElementById('balancaId');
@@ -20,8 +21,8 @@ function create(peso, medidaEm, balancaId){
     return firebase.database().ref().child('medidas').push(data);
 }
 
-// Função geradora de uma listagem com as medidas no banco de dados
-mostraMedidas = function (){
+// Função geradora de uma listagem com as medidas no banco de dadosc
+function mostrarMedidas (){
 firebase.database().ref('medidas').on('value',function (snapshot) {
     medsList.innerHTML = '';
      snapshot.forEach(function (item) {
@@ -33,24 +34,15 @@ firebase.database().ref('medidas').on('value',function (snapshot) {
     });
 }
 
-/* // Função geradora de uma tabela com as medidas no banco de dados
-firebase.database().ref('medidas').on('value',function (snapshot) {
-    medsTable.innerHTML = '';
-     snapshot.forEach(function (item) {
-        var tr = document.createElement('TABLE');
-        tr.appendChild(document.createTextNode(
-        <td> item.val().medidaEm) </td>
-         + <td> item.val().peso </td> 
-         + <td> item.val().scaleId </td>
-         ));
-        medsTable.appendChild(tr);
-    }); 
-}); */
-
-
-{/* <tbody>
-        <tr class="table-active">
-          <th scope="row">Active</th>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr> */}
+// Função geradora de uma listagem com as balancas no banco de dados
+ function mostrarLixeiras (){
+    firebase.database().ref('balancas').on('value',function (snapshot) {
+        balList.innerHTML = '';
+         snapshot.forEach(function (item) {
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(item.val().local + ': ' 
+             + item.val().apelido + ': ' + item.val().dono));
+            balList.appendChild(li);
+            }); 
+        });
+    }
