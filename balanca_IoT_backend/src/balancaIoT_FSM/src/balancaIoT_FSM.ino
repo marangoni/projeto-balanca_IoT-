750f7f117335990abe0afbef01a63122da5d98f1
marangoni <oliveiralcm@gmail.com>
2021-03-10 20:47:12 -0300

    // Arquivo: balanca_IoT_FSM // Autor: Luiz C M Oliveira // Descricao: //    - Fator de calibração correto //    - Implementacao de rotina check_RESET - RESET após 10s de medidas zeradas //    - Correção do hook.json para criação da integração //    - Criação da rotina para montar o json quando houver o reset //    - Inclusão de rotinas para detecção de nova medida e de reset //    - Limitação do número de pubicações repetidas // Atualização - 10.03.2020:

--- balanca_IoT_backend/src/balancaIoT_FSM/src/balancaIoT_FSM.ino
+++ balanca_IoT_backend/src/balancaIoT_FSM/src/balancaIoT_FSM.ino
@@ -1,10 +1,13 @@
 // Arquivo: balanca_IoT_FSM
 // Autor: Luiz C M Oliveira
 // Descricao: 
-//    - Atualização do json para publicação das medidas POR DISPOSITIVO
-//    - Correção do banco de dados - firebase
+//    - Fator de calibração correto
+//    - Implementacao de rotina check_RESET - RESET após 10s de medidas zeradas
 //    - Correção do hook.json para criação da integração
-// Atualização - 08.03.2020: 
+//    - Criação da rotina para montar o json quando houver o reset
+//    - Inclusão de rotinas para detecção de nova medida e de reset
+//    - Limitação do número de pubicações repetidas
+// Atualização - 10.03.2020: 
 //
 
 #include "Particle.h"
@@ -20,22 +23,35 @@ LiquidCrystal_I2C *lcd;
 
 HX711ADC scale(DT,SCK);
 
-//fator de calibração     
-float calibration_factor = 17130;
+//fator de calibração ***** Importante para medidas corretas. Verificar conforme a balanca     
+float calibration_factor = 13090; //balanca 150 kg - balanca_iot2
+//float calibration_factor = 1132650; //balanca 1kg - GEPIC1
 
 const unsigned long PUBLISH_PERIOD_MS = 60000;  //periodo entre publicações 60s
 const unsigned long FIRST_PUBLISH_MS = 5000;    //primeira publicacao com 5s
 unsigned long lastPublish = FIRST_PUBLISH_MS - PUBLISH_PERIOD_MS;
 
+const unsigned long PERIODO_MEDIDA_MS = 5000;  //periodo entre medidas 5s   //versao final ajustar este valor para um intervalo maior
+const unsigned long PRIMEIRA_MEDIDA = 500;     //primeira medida 0,5s
+unsigned long lastMed = PRIMEIRA_MEDIDA - PERIODO_MEDIDA_MS;
+
+const unsigned long PERIODO_ULTIMO_RESET_MS = 60000;  //periodo verificacao RESET - 60s (versao final ajustar valor para 2h ou mais)
+const unsigned long PRIMEIRO_RESET = 500;             //primeiro reset 0,5s
+unsigned long lastRST = PRIMEIRO_RESET - PERIODO_ULTIMO_RESET_MS;
+
 double medida = 0;
 double medida_anterior = 0;
 bool nova_medida = false;           //Flag que indica que houve alteração na medida
+bool reset_da_balanca = false;
 int startTime = 0;
+int count = 0;                      //contador 
+
 
 //Dados para identificação da balanca no BD
 String deviceName;
 
 String Data;
+String Data_reset;  
 
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
@@ -72,12 +88,13 @@ int init_scale();                           // inicializa balança
 int config_display();                       // configuraçao do display LCD - peso e kg
 int msg_inicial();                          // abertura do programa 
 int montar_json();                          // montagem do json para publicacao
+int montar_json_reset();                    // montagem do json de reset da balanca
 int atualiza_display(String valor);         // atualizaçao da medida no display
 bool check_med(double med, double med_ant); // verificacao de nova medida
 int salva_medida_EEPROM(double med);        // salva nova medida na eeprom
 double le_medida_EEPROM();                  // le medida na eeprom
 void deviceNameHandler(const char *topic, const char *data);        //cria nome do dispositivo para json
-
+bool check_reset(double med, double med_ant); // verifica se houve reset
 
 void setup() {
     Log.info("Executando void setup()...");
@@ -92,7 +109,7 @@ void setup() {
     Particle.connect();      // Conecta particle a nuvem
     Time.zone(-3.00);        // Ajusta horário conforme horário brasileiro - GMT-3h
     stateTime = millis();
-    estado = ESTADO_RESET_SCALE;  // NESSA versao a balanca deve iniciar zerada, na versao final reset por evento
+    estado = ESTADO_INIT;  //Inicializa a balanca
 }
 
 void loop() {
@@ -135,26 +152,54 @@ void loop() {
              // Há como melhorar este código... esta bloqueando o programa o tempo todo 
              // para verificar nova_medida (PROXIMAS MELHORIAS)
 
-             medida = (scale.get_units(10));
+             medida = (scale.get_units(3));
              if(medida<0){medida=0;}                           //condiciona medidas negativas
              
-             nova_medida = check_med(medida, medida_anterior); //verifica se é nova medida ou ruido
+            Log.info("medida " + String(medida));
+            Log.info("medida anterior" + String(medida_anterior));
              
-            if(nova_medida==true)
+             
+             if (millis() - lastMed >= PERIODO_MEDIDA_MS) {   //a cada 5s verifica se é nova medida ou nao e se zerou ou nao
+		            lastMed = millis();
+		            Log.info("Verifica se eh nova medida ou nao");
+                    nova_medida = check_med(medida, medida_anterior);
+                    if(medida ==0){ //Se medida for zero, repita 3x, se continuar zero - atualize eeprom
+                        count++;
+                        if (count ==3){
+                            Log.info("Medida zerada na EEPROM");
+                            salva_medida_EEPROM(medida);
+                            count = 0; 
+                        }
+                    }
+             }      
+
+             if (millis() - lastRST >= PERIODO_ULTIMO_RESET_MS) {   //a cada 60s verifica se houve reset ou nao
+                    lastRST = millis();
+		            Log.info("Verificando se houve reset...");
+                    reset_da_balanca = check_reset(medida, medida_anterior);    //sinaliza se houve ou nao reset
+            }     
+            
+            if((nova_medida==true) && (medida!=0)) //somente publica se a medida for nova e DIFERENTE DE ZERO
              {
                 Log.info("Nova medida detectada...");  
                 atualiza_display(String(medida,2));
                 salva_medida_EEPROM(medida);
                 estado = ESTADO_PUBLICA_EVENTO;
-                Log.info("Indo para estado_em_publicando"); 
+                Log.info("Indo para estado_publica_evento"); 
              }
-             else
+             else if(reset_da_balanca == true)  //se ocorreu o RESET (esvaziamento) da lixeira, vá para o estado reset
+             {
+                reset_da_balanca = false;       //consome o flag
+                Log.info("Reset da balanca");
+                estado = ESTADO_RESET_SCALE;
+             }
+             else                               //se nada ocorreu, continua em operacao
              {
                 estado = ESTADO_EM_OPERACAO;
                 Log.info("Fica no estado em operação"); 
              }
-             
-            break;
+  
+        break;
 
         case ESTADO_PUBLICA_EVENTO: 
             Log.info("ESTADO_PUBLICA_EVENTO"); 
@@ -171,19 +216,16 @@ void loop() {
                 }
                 stateTime = millis();
             }
-            // When checking the future, the isDone() indicates that the future has been resolved, 
-            // basically this means that Particle.publish would have returned.
-            if (publishFuture.isDone()) {
+            
+            if (publishFuture.isDone()) {               //Se houve resposta ao Particle.publish executa abaixo, se não, sai
                 Log.info("Retorno da funcao publish");
-                // isSucceeded() is whether the publish succeeded or not, which is basically the
-                // boolean return value from Particle.publish.
-                if (publishFuture.isSucceeded()) {
+                
+                if (publishFuture.isSucceeded()) {      //Se a publicacao foi bem sucedida
                     Log.info("publicacao ok");
-                    //estado = ESTADO_AGUARDE_1S;
                     estado = ESTADO_EM_OPERACAO;
                     Log.info("Indo para estado_em_operacao"); 
                 }
-                else {
+                else {                                  //se a publicação nao foi bem sucedida
                     Log.info("falha na publicacao, medida perdida");
                     estado = ESTADO_SLEEP;
                     Log.info("Indo para estado_SLEEP"); 
@@ -214,10 +256,10 @@ void loop() {
             estado = ESTADO_EM_OPERACAO; //correto voltar para o init
             Log.info("Indo para estado_em_operacao"); 
             stateTime = millis();
-            break; 
+        break; 
 
         case ESTADO_RESET_SCALE:
-            Log.info("EStADO_RESET_SCALE"); 
+            Log.info("ESTADO_RESET_SCALE"); 
             medida = 0;
             medida_anterior = 0;
             salva_medida_EEPROM(medida);
@@ -227,24 +269,26 @@ void loop() {
             lcd->setCursor(4,1);
             lcd->print("lixeira");
             delay(3000);        //remover para não bloquear o programa
-            estado = ESTADO_INIT;
-            Log.info("Indo para estado_init"); 
-         break;
+            
+            //publica evento reset
+            montar_json_reset();
+            
+            //Particle.publish("Reset da balanca");
+            //publishFuture = Particle.publish("novo_reset", Data_reset, PRIVATE | WITH_ACK);
+             if (millis() - lastPublish >= PUBLISH_PERIOD_MS) {
+		         lastPublish = millis();
+		         if (deviceName.length() > 0) {
+                    
+                     publishFuture = Particle.publish("novo_reset", Data_reset, PRIVATE | WITH_ACK);
+                     Log.info("publicando evento reset...", Data_reset);
+                 }
+            //     //stateTime = millis();
+             }
+            
+            estado = ESTADO_EM_OPERACAO;
+            Log.info("Indo para estado_em_operacao"); 
+        break;
 
-        //  case ESTADO_AGUARDE_1S:
-        //  // nao utilizado
-        //  Log.info("ESTADO_AGUARDE_1S"); 
-        //     //stateTime = millis();
-        //     if(millis()-stateTime>3000){ 
-        //         Log.info("Indo para estado_em_operacao"); 
-        //         estado = ESTADO_EM_OPERACAO;
-        //     }
-        //     else{
-        //         Log.info("Indo para estado_aguarde_1s"); 
-        //         estado = ESTADO_AGUARDE_1S;
-        //     }
-
-        //  break;
     }
 }
 
@@ -338,21 +382,52 @@ int montar_json(){
     Data+="\"n\":\"" + String(deviceName.c_str())+"\"}";			//nome do dispositivo    
     
  return 0;
-	//Particle.publish("medidas", Data, PUBLIC);   
+	
+}
+
+int montar_json_reset(){
+    // Monta o json com a informacao de data do último reset para publicacao no evento - novo_reset
+    // Nesta versao está sobreescrevendo os campos do BD - verificar webhook com o PUT
+
+    String timeStamp = Time.timeStr();
+    timeStamp = Time.format("%d/%m/%y %H:%M:%S");
+  
+    Data_reset="{\"ultimoReset\":\""+ String(timeStamp) +"\",";              //ultimoReset
+    Data_reset+="\"n\":\"" + String(deviceName.c_str())+"\"}";               //nome do dispositivo
+            
+ return 0;
+	  
 }
 
 bool check_med(double med, double med_ant){
     int delta = 0.2; //variação detectável de peso 200g 
 
-    // verifica se nova medida não foi pela compressão do lixo - aguarda 5s
-    delay(5000);
-
-    if((med > med_ant + delta)){// se houve aumento do peso - nova medida
+    if((med > med_ant + delta)||(med==0)){// se houve aumento do peso - nova medida OU medida zerou
         return true;
         }
         else{
             return false;
         }
+}
+
+bool check_reset(double med, double med_ant){
+Log.info("check_reset");
+if(med<0.001){med=0;}
+    if(med==0 && med_ant==0){
+        delay(3000);
+        med = scale.get_units(3); //Aós 3s verifica balanca novamente
+        if(med<0.001){med=0;}
+        Log.info("check_reset");
+        Log.info(String(med,2));
+        if(med==0){
+            Log.info("reset detectado na check_reset");
+            return true; //se após 5s a medida ainda for 0. RESET
+        }
+        else{
+            Log.info("reset não detectado na check_reset");
+            return false; //se não. Não é reset
+        }
+    }
 }
 
 int salva_medida_EEPROM(double med)
